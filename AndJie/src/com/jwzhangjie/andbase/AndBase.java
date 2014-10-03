5ccf93747ec33ecb4843f236a534d8e424495241
jwzhangjie <jwzhangjie@163.com>
2014-10-03 13:15:20 +0800

    add new project

---
+++ AndJie/src/com/jwzhangjie/andbase/AndBase.java
@@ -0,0 +1,15 @@
+package com.jwzhangjie.andbase;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AndBase extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_and_base);
+	}
+}
